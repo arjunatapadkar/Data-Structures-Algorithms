06ddc57942cb8f0bd970b2655c6007d729aab77f
Arjun Atpadkar <arjunatapadkar94@gmail.com>
2023-04-18 08:58:33 +0530

    strings basics

---
+++ week 5 Char Arrays and Strings/class 1/string.cpp
@@ -0,0 +1,45 @@
+
+#include<iostream>
+#include<string>
+using namespace std;
+
+int main(){
+
+    string str = "Arjun Atapadkar";
+    // cout << str << endl;
+
+    string s  = "Arjun";
+    // cin >> s;
+    // getline(cin, s);
+
+    // cout << s << endl; 
+
+    cout << str[3] << " " << s[2] << endl;
+
+
+    cout << str.length() << endl;
+
+    cout << str.empty() << endl;
+
+    cout << str.find("Arjun") << endl;
+
+    str.push_back('A') ;
+    cout << str << endl;
+
+    str.pop_back();
+    cout << str << endl;
+
+    cout << str.substr(0, 5) << endl;
+    cout << str.substr(0, 5) << endl;
+
+    cout << str.compare(s) << endl;
+    cout << s.compare(str) << endl;
+
+    str.replace(6, 10, "Atp");
+    cout << str << endl;
+
+    str.erase(6, 3) ;
+    cout << str << endl;
+
+    return 0;
+}
